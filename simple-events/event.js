2e814860cff869d1d68662edc0b5309a260fb7a8
apecr <alberto.eyo@gmail.com>
2020-04-24 17:24:08 +0200

    feat() simple event kata

--- simple-events/event.js
+++ simple-events/event.js
@@ -1,5 +1,16 @@
 function Event() {
-  //your implementation goes here
+  let handlers = []
+  return {
+    subscribe(f) {
+      handlers.push(f)
+    },
+    unsubscribe(f) {
+      handlers = handlers.filter(handler => handler !== f)
+    },
+    emit() {
+      handlers.forEach(handler => handler.call(null, ...arguments))
+    }
+  }
 }
 
 module.exports = Event
